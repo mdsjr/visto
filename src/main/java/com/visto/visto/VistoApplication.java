d1031d86838d10ea0513534ef57fc8de80d22c14
Junior <moacirjr>
2026-03-26 22:36:31 -0300

    feat: configurar estrutura base do projeto Spring Boot

---
+++ src/main/java/com/visto/visto/VistoApplication.java
@@ -0,0 +1,13 @@
+package com.visto.visto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visto/visto/VistoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visto.visto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
